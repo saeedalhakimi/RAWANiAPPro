7f9a749c962bd05ebe7f6cf1bc6216fdc79081d7
Saeed Al hakimi <Saeed.alhakimi@live.com>
2025-01-21 08:20:38 +0300

    LoginCommand

---
+++ RAWANi.WEBAPi.Application/MEDiatR/AuthMDIR/Commands/LoginCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAWANi.WEBAPi.Application.MEDiatR.AuthMDIR.Commands
+{
+    internal class LoginCommand
+    {
+    }
+}
