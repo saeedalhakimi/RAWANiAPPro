ec2e3b3056cfed456794bf99d69c4dc54491df5e
Saeed Al hakimi <Saeed.alhakimi@live.com>
2025-01-21 08:33:06 +0300

    removed www from track

--- RAWANi.WEBAPi.Application/MEDiatR/AuthMDIR/Commands/LoginCommand.cs
+++ RAWANi.WEBAPi.Application/MEDiatR/AuthMDIR/Commands/LoginCommand.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MediatR;
+using RAWANi.WEBAPi.Application.Contracts.AuthDtos.Responses;
+using RAWANi.WEBAPi.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace RAWANi.WEBAPi.Application.MEDiatR.AuthMDIR.Commands
 {
-    public class LoginCommand
+    public class LoginCommand : IRequest<OperationResult<ResponseWithTokensDto>>
     {
     }
 }
